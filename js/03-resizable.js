fd976e2a7c41dda30360423ccea3b75db5c3daaa
kuomolew <kuomolew@gmail.com>
2019-12-06 22:14:33 +0200

    finish

--- js/03-resizable.js
+++ js/03-resizable.js
@@ -1,20 +1,26 @@
 //Создать HTML-страницу с блоком текста в рамочке. Реализовать возможность изменять размер блока, если зажать мышку в правом нижнем углу и тянуть ее дальше.
 
 $(document).ready( function () {
-    
-    
+    let pressed = false;
+    $('body').on('click', '.corner', (e) => {
+        pressed = false;
+    });  
     $('body').on('mousedown', '.corner', (e) => {
-        console.log('Start');
         let initialHeight = $('.resizable').outerHeight();
         let initialWidth = $('.resizable').outerWidth();
         let x = e.pageX; 
         let y = e.pageY; 
-        $(document).mousemove(function(e){
-            let width = initialWidth + (e.pageX - x);
-            let height = initialHeight + (e.pageY - y);
-            $('.resizable').css('width', `${width}px`)
-                            .css('height', `${height}px`);
+        pressed = true;
+        $(document).mousemove(function(event){
+            $('body').on('mouseup', () => {
+                pressed = false;
+            });
+            if (pressed) {
+                let width = initialWidth + (event.pageX - x);
+                let height = initialHeight + (event.pageY - y);
+                $('.resizable').css('width', `${width}px`)
+                                .css('height', `${height}px`);
+            }
         });     
     });
-
-} );
+});
